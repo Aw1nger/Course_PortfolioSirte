0417f3505347a72905443f5e394f3841eeff131c
Nikita <n.borodin@nekto-z.ru>
2024-04-01 21:16:31 +0300

    initial commit!

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import AppRouter from "./pages/AppRouter";
+
+function App() {
+  return <AppRouter />;
+}
+
+export default App;
